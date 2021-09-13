7fe63475b2561f84fe8d3ccf3e95c1091fe25202
Mladen Mikić <m.mikic@rooom.com>
2021-09-13 10:13:42 +0200

    Initial Commit

---
+++ SwiftUI_Coordinator_Pattern_Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Coordinator_Pattern_Example
+//
+//  Created by Mladen Mikic on 13.09.2021..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
